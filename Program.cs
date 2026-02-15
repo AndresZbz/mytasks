f9f49b31f155182dccd80ec0dd66454780e7648e
AndresZbz <juanandresbouzab@gmail.com>
2026-02-15 00:32:29 -0400

    basic rbac endpoints, index.html wwwroot file added

--- Program.cs
+++ Program.cs
@@ -112,7 +112,21 @@ if (app.Environment.IsDevelopment())
 //app.UseHttpsRedirection();
 
 //routes
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async (HttpContext context) =>
+{
+    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html");
+    
+    if (File.Exists(filePath))
+    {
+        context.Response.ContentType = "text/html";
+        await context.Response.SendFileAsync(filePath);
+    }
+    else
+    {
+        context.Response.StatusCode = 404;
+        await context.Response.WriteAsync("File not found");
+    }
+});
 app.MapTasksRoutes();
 app.MapUserRoutes();
 app.MapStatusRoutes();
--- routes/Rbac.cs
+++ routes/Rbac.cs
@@ -6,7 +6,14 @@ public static class Rbac
     {
         var group = app.MapGroup("/api/rbac");
 
+        //roles
         group.MapGet("/roles/", GetAllRoles);
+
+        //permissions
+        group.MapGet("/permissions/", GetAllPermissions);
+
+        //role_permissions
+        group.MapGet("/role_permissions/", GetAllRolePermissions);
     }
 
     //Roles endpoints
@@ -14,5 +21,19 @@ public static class Rbac
     {
         var roles = await db.Role.ToListAsync();
         return Results.Ok(roles);
-    } 
+    }
+
+    //Permissions endopints
+
+    public static async Task<IResult> GetAllPermissions(AppDbContext db)
+    {
+        var perms = await db.Permission.ToListAsync();
+        return Results.Ok(perms);
+    }
+
+    public static async Task<IResult> GetAllRolePermissions(AppDbContext db)
+    {
+        var role_perms = await db.RolePermissions.ToListAsync();
+        return Results.Ok(role_perms);
+    }
 }
